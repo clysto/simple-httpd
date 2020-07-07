ef0168d6f64059b3cf70f934ceecf28e8615206f
毛亚琛 <maoyachen55@gmail.com>
2020-07-07 15:46:24 +0800

    日常更新
    - 区分了GET方法
    - Request中的body类型改为bytes
    - 增加了RawResponse类，CGI脚本的输出封装为RawResponse
    - 增加了post.cgi测试脚本

--- response.py
+++ response.py
@@ -1,6 +1,23 @@
 from datetime import datetime
 from http_status import HttpStatus
 
+WEEKDAY = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+MONTH = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
+
+
 class Response:
     def __init__(self, body):
         self.body = body
@@ -8,25 +25,12 @@ class Response:
         self.header = {
             "Date": self.format_date(datetime.utcnow()),
             "Contene-Length": len(body),
-            "Server": "simple-httpd/0.0.1"
+            "Server": "simple-httpd/0.0.1",
         }
 
     def format_date(self, dt):
-        weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt.weekday()]
-        month = [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-        ][dt.month - 1]
+        weekday = WEEKDAY[dt.weekday()]
+        month = MONTH[dt.month - 1]
         return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (
             weekday,
             dt.day,
@@ -49,3 +53,18 @@ class Response:
 
     def set_status(self, status):
         self.status = status
+
+
+class RawResponse:
+    def __init__(self, content: bytes):
+        self.status = HttpStatus.OK
+        self.content = content.replace(b"\r", b"")
+        self.content = content.replace(b"\n", b"\r\n")
+
+    def set_status(self, status):
+        self.status = status
+
+    def encode(self):
+        status = self.status.value
+        response_line = f"HTTP/1.1 {status[0]} {status[1]}\r\n".encode()
+        return response_line + self.content
